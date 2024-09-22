637ad0513e34eca5efb1bec49b5702978cf3cd56
Nashtradamus <Nashmoore23@gmail.com>
2024-09-22 10:07:48 -0500

    Updated comments

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 def main():
+   #VARS
+   #book_path > text > character_count_dict > character_sorted_list
    book_path = "books/frankenstein.txt"
    text = get_book_text(book_path)
    w_count = word_count(text)
    character_count_dict = char_count_dict(text)
    character_sorted_list = char_dict_to_sorted_list(character_count_dict)
 
+
+   #FORMATING OUTPUT
    print(f"--- Begin report of {book_path} ---")
    print(f"{w_count} words found in the document")
    print()
 
    for i in character_sorted_list:
-       if not i["char"].isalpha():
-           continue #never showed this in lessons
-       print(f"The '{i['char']}' character was found '{i['num']}' times")#never covered the difference between signle and double quotes. Simply stated to be consistent and always use double quotes
-       #also never explained why you don't have to use single quotes around second set of elip for i['num']
+       if not i["char"].isalpha():#checks if character is a letter
+           continue
+       print(f"The '{i['char']}' character was found '{i['num']}' times")
 
-   print("--- End report ---") #had to shift + left arrow to delete space that was getting auto inserted at beginning or I get an indentation error, this was never covered
+   print("--- End report ---") 
 
+#FUNCTIONS
 def get_book_text(path):
     with open(path) as f:
         return f.read()
             
-def word_count(string):
+def word_count(string): #could use a .length here
     words = string.split()
     count = 0
     for i in words:
             count += 1
     return(count)
 
-def char_count_dict(string):   
+def char_count_dict(string):#convert letters to lowercase and add them to a dict with key:value   
     lower_string = string.lower()
     dictionary = {}
     for i in lower_string:
@@ -38,16 +42,17 @@ def char_count_dict(string):
              dictionary[i] = 1
     return(dictionary)
 
-def sort_on(dict):  
-    return dict["num"]
-
 def char_dict_to_sorted_list(dict):
     list = []
     for i in dict:
-        list.append({"char": i, "num": dict[i]})
-
-    list.sort(reverse=True, key=sort_on) #never showed "sort" in lessons, mentions it for first time in this project
-    #every other time we have called a funtion you have to pass a value but for some reason with sort you don't have to when using "key="
+        list.append({"char":i, "num":dict[i]})
+    list.sort(reverse = True, key=sort_on)
+    list.sort()
     return list
+
+def sort_on(dict):  
+    return dict["num"]
+
+
 
 main()
